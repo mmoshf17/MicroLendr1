bad0f7baf480e79374f7e9eb9726d684a7b3f741
mohibmoshfiq <mohibkhan@hotmail.com>
2019-12-11 18:39:43 +0100

    Added more features (GDPR)

--- app/src/main/java/com/example/mohib/microlendr/TermsConditions.java
+++ app/src/main/java/com/example/mohib/microlendr/TermsConditions.java
@@ -1,7 +1,11 @@
 package com.example.mohib.microlendr;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class TermsConditions extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class TermsConditions extends AppCompatActivity {
         setContentView(R.layout.activity_terms_conditions);
 
 
+    }
+
+    public void onClickTermsBtn(View view) {
+
+        TextView txtGDPRLink = findViewById(R.id.textGDPR);
+
+        String url = txtGDPRLink.getText().toString();
+        Intent goToWeb = new Intent(Intent.ACTION_VIEW);
+        goToWeb.setData(Uri.parse(url));
+        startActivity(goToWeb);
     }
 }
